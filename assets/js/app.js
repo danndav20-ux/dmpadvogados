04da8ce55da048672f891e378bdbb0ef503b097e
danndav20-ux <danndav20@gmail.com>
2025-08-09 11:45:29 -0300

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.nav a[href^="#"]').forEach(a=>{
-  a.addEventListener('click',e=>{
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth',block:'start'});
-  });
-});
